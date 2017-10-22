3ef828dfec3ddcec3ce975c2ae02c94895c0c852
Ethan Brown <ethanbrown3@mail.weber.edu>
2017-10-21 18:05:36 -0600

    index updates

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ export default class App extends Component {
                     debug // debug mode will show feature points detected and 3D axis
                     planeDetection // turn on plane detection
                     lightEstimation // turn on light estimation
-                ></ARKit>
+                >
                     <ARKit.Box
                         pos={{ x: 0, y: 0, z: 0 }}
                         shape={{ width: 0.1, height: 0.1, length: 0.1, chamfer: 0.01 }}
@@ -19,12 +19,12 @@ export default class App extends Component {
                         pos={{ x: 0.4, y: 0, z: 0 }}
                         shape={{ radius: .05, height: 0.1 }}
                     />
-                
-                <ARKit.Pyramid
-                    pos={{ x: 0.2, y: 0.15, z: 0 }}
-                    shape={{ width: 0.1, height: 0.1, length: 0.1 }}
-                />
-            </View>
+                    <ARKit.Pyramid
+                        pos={{ x: 0.2, y: 0.15, z: 0 }}
+                        shape={{ width: 0.1, height: 0.1, length: 0.1 }}
+                    />
+                </ARKit>
+            </View> 
         );
     }
 }
